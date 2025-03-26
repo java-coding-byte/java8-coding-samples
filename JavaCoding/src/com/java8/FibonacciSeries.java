18628f803e21fdb8dfdbc9db0c3eb9d252d6719e
Sreenivasa <Sreenivasa@host.docker.internal>
2025-03-26 12:25:18 -0400

    Updated file

--- JavaCoding/src/com/java8/FibonacciSeries.java
+++ JavaCoding/src/com/java8/FibonacciSeries.java
@@ -5,7 +5,7 @@ import java.util.stream.Stream;
 public class FibonacciSeries {
 	
 	public static void main(String[] args) {
-		
+		System.out.println("Fibannoic series");
 		Stream.iterate(
 				new long[]{0, 1},
 				fib -> new long[]{fib[1], fib[0] + fib[1]})
