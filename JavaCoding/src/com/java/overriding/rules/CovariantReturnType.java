f6689fb4c85f9f292c90a3c30ae151914ed7e8d3
java-coding-byte <sreenivasarao.aripaka@gmail.com>
2025-04-18 18:38:51 -0400

    Covariant Type

--- JavaCoding/src/com/java/overriding/rules/CovariantReturnType.java
+++ JavaCoding/src/com/java/overriding/rules/CovariantReturnType.java
@@ -1,5 +1,30 @@
 package com.java.overriding.rules;
 
+class A {
+	public A giveBirth() {
+		return new A();
+	}
+}
+
+class B extends A {
+	@Override
+	public B giveBirth() {
+		return new B();
+	}
+
+}
+
 public class CovariantReturnType {
+
+	public static void main(String[] args) {
+
+		A a = new A();
+		A ab = a.giveBirth();
+		System.out.println(ab.getClass()); // Output: class A
+
+		B b = new B();
+		B bb = b.giveBirth();
+		System.out.println(bb.getClass()); // Output: class B
+	}
 
 }
